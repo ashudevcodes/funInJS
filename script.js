53feee7184e785a96d97f6eb3b554974e3113587
ashudevcodes <ashishprasad949@gmail.com>
2025-09-21 20:47:40 +0530

    baaaaa | aaaaa
    play meme sound on hover
    SVG animation

---
+++ script.js
@@ -0,0 +1,60 @@
+console.log(clickMe)
+
+let timesToEnter = 0
+
+aaMemeSound = new Audio("aaMemeAudio.mp3")
+baaMemeAudio = new Audio("baaMemeAudio.mp3")
+ohMyGodMemeAudio = new Audio("ohMyGodMemeAudio.mp3")
+gotYou = new Audio("gotYou.mp3")
+
+function playSound(sound) {
+  sound.currentTime = 0
+  sound.preservesPitch = false
+  sound.play()
+}
+
+
+const eventTable = [
+  {
+	onCount: 0,
+	action: () => {
+	  playSound(aaMemeSound)
+	}
+  },
+  {
+	onCount: 5,
+	action: () => {
+	  playSound(ohMyGodMemeAudio)
+	}
+  },
+  {
+	onCount: 8,
+	action: () => {
+	  playSound(gotYou)
+	}
+  },
+  {
+	onCount: 10,
+	action: () => {
+	  playSound(aaMemeSound)
+	}
+  },
+]
+
+eventTable.sort((a, b) => b.onCount - a.onCount);
+
+clickMe.onmouseenter = () => {
+  timesToEnter +=1
+  for (const event of eventTable){
+	if (event.onCount <= timesToEnter) {
+	  event.action()
+	  break
+	}
+  }
+}
+
+clickMe.onmouseleave = () => {
+  console.log("sayonara!")
+  baaMemeAudio.currentTime = 0 
+  baaMemeAudio.play()
+}
