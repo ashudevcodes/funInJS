6be512965f8736c6de52dbeb46b96a43ef248591
ashudevcodes <ashishprasad949@gmail.com>
2025-09-21 21:28:29 +0530

    Got You Homie!!
    Add SVG Text data and Counter

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ baaMemeAudio = new Audio("baaMemeAudio.mp3")
 ohMyGodMemeAudio = new Audio("ohMyGodMemeAudio.mp3")
 gotYou = new Audio("gotYou.mp3")
 
+
 function playSound(sound) {
   sound.currentTime = 0
   sound.preservesPitch = false
@@ -19,24 +20,28 @@ const eventTable = [
 	onCount: 0,
 	action: () => {
 	  playSound(aaMemeSound)
-	}
+	  playCount.innerText = `Aaaaaaaaaaaa!! ${timesToEnter}`
+	},
   },
   {
 	onCount: 5,
 	action: () => {
 	  playSound(ohMyGodMemeAudio)
-	}
+	  playCount.innerText = `Oh My God!! ${timesToEnter}`
+	},
   },
   {
 	onCount: 8,
 	action: () => {
 	  playSound(gotYou)
+	  playCount.innerText = `Got You Homie!! ${timesToEnter}`
 	}
   },
   {
 	onCount: 10,
 	action: () => {
 	  playSound(aaMemeSound)
+	  playCount.innerText = `Aaaaaaaaaaaa!! ${timesToEnter}`
 	}
   },
 ]
